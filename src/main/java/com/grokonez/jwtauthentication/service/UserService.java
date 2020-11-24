eebdc19684a314e060ad739133c08345cdf33c36
Hamza SOUIRI <hamza.souiri97@gmail.com>
2020-11-24 10:07:01 +0100

    basic role based authentication with spring security and jwt

---
+++ src/main/java/com/grokonez/jwtauthentication/service/UserService.java
@@ -0,0 +1,26 @@
+package com.grokonez.jwtauthentication.service;
+
+import com.grokonez.jwtauthentication.model.User;
+import com.grokonez.jwtauthentication.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException(username));
+    }
+}
